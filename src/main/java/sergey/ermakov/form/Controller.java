87afae04cd04e5c0840c7cc1ac4a650bc52aa411
sergeyermakov239 <sergeyermakov13112004@gmail.com>
2021-12-19 19:05:25 +0300

    SUPER FORM!!!

--- src/main/java/sergey/ermakov/form/Controller.java
+++ src/main/java/sergey/ermakov/form/Controller.java
@@ -6,14 +6,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 @RestController
 public class Controller {
     ArrayList<form> arr=new ArrayList<>();
 
     @PostMapping("form/post")
-    public void postform(@RequestBody form Form){
+    public void postform(@RequestBody form Form) {
         arr.add(Form);
+        Collections.sort(arr, new Comparator<form>() {
+            @Override
+            public int compare(form o1, form o2) {
+                if (o1.getName().compareTo(o2.getName())>0){
+                    return 1;
+                } else {
+                    return  -1;
+                }
+            }
+        });
     }
 
     @GetMapping("form/get")
--- src/main/resources/tic.js
+++ src/main/resources/tic.js
@@ -4,23 +4,32 @@ function submit(){
     Form.email=document.getElementById('email').value;
     Form.phone=document.getElementById('phone').value;
     Form.questions=document.getElementById('questions').value;
-    $.ajax({
-        url:"http://localhost:8080/form/post",
-        type:'POST',
-        contentType:"application/json;charset=utf-8",
-        data:JSON.stringify(Form),
-        success:function (){
-            window.alert('SUCCESS');
-        },
-        error:function (e) {
-            window.alert('ERROR');
-        }
-    });
-    document.getElementById('name').value='';
-    document.getElementById('email').value='';
-    document.getElementById('phone').value='';
-    document.getElementById('questions').value='';
-
+    if (Form.name===''){
+        window.alert('Поле "Имя" должно быть заполнено');
+    } else if (Form.email===''){
+        window.alert('Поле "Почта" должно быть заполнено');
+    } else if (Form.phone===''){
+        window.alert('Поле "Мобильный телефон" должно быть заполнено');
+    } else if (Form.questions===''){
+        window.alert('Поле "Вопросы" должно быть заполнено');
+    } else {
+        $.ajax({
+            url: "http://localhost:8080/form/post",
+            type: 'POST',
+            contentType: "application/json;charset=utf-8",
+            data: JSON.stringify(Form),
+            success: function () {
+                window.alert('SUCCESS');
+            },
+            error: function (e) {
+                window.alert('ERROR');
+            }
+        });
+        document.getElementById('name').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('phone').value = '';
+        document.getElementById('questions').value = '';
+    }
 }
 
 function usertable(){
